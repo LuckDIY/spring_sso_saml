521224f0850185c079c185159e6ed29306cbd574
chaoleiwang <1905943612@qq.com>
2025-06-05 11:12:11 +0800

    sp新增自己的oidc单点登录服务

--- src/main/java/com/chaolei/wang/spring_sso_saml/security/SecurityConfiguration.java
+++ src/main/java/com/chaolei/wang/spring_sso_saml/security/SecurityConfiguration.java
@@ -10,12 +10,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.saml2.Saml2LoginConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableMethodSecurity
@@ -67,6 +69,7 @@ public class SecurityConfiguration {
             }
         });
 
+        http.csrf(AbstractHttpConfigurer::disable);
         http.oauth2Login(oauth2 -> oauth2
                 .defaultSuccessUrl("/home")
         );
